a667baf88d41319010b5f72cbef85f0f3e80ea6c
Wim Looman <wim@nemo157.com>
2018-05-28 11:27:01 +0200

    Add another layer to support non-UI events

--- src/client.rs
+++ src/client.rs
@@ -23,32 +23,44 @@ pub struct Action {
 }
 
 #[derive(Debug)]
+enum Event {
+    Ui(Action),
+}
+
+#[derive(Debug)]
 struct State {
+    handle: Handle,
     count: usize,
     msg: String,
 }
 
 impl State {
-    fn with_channels(mut self, rx: mpsc::Receiver<Action>, tx: mpsc::Sender<VNode<Action>>) -> impl Future<Item = (), Error = ()> {
-        tx.send(self.render())
+    fn spawn(handle: Handle) -> (impl Sink<SinkItem = Event, SinkError = ()>, impl Stream<Item = VNode<Action>, Error = ()>) {
+        let (events_tx, events_rx) = mpsc::channel(1);
+        let (render_tx, render_rx) = mpsc::channel(1);
+        let mut state = State { handle: handle.clone(), count: 0, msg: "".into() };
+        handle.spawn(render_tx.send(state.render())
             .map_err(|e| println!("state send error: {:?}", e))
-            .and_then(|tx| rx
+            .and_then(|tx| events_rx
                 .fold(tx, move |tx, action| {
-                    if self.update(action) {
-                        Either::A(tx.send(self.render()).map_err(|e| println!("state send error: {:?}", e)))
+                    if state.update(action) {
+                        Either::A(tx.send(state.render()).map_err(|e| println!("state send error: {:?}", e)))
                     } else {
                         Either::B(future::ok(tx))
                     }
                 })
-                .map(|_| ()))
+                .map(|_| ())));
+        (events_tx.sink_map_err(|e| println!("error sinking event: {:?}", e)), render_rx)
     }
 
-    fn update(&mut self, action: Action) -> ShouldRender {
-        println!("action: {:?}", action);
-        match action.tag {
-            ActionTag::Increment => self.count += 1,
-            ActionTag::Decrement => self.count -= 1,
-            ActionTag::SubmitMsg => self.msg = action.associated.get("value").unwrap().clone(),
+    fn update(&mut self, event: Event) -> ShouldRender {
+        match event {
+            Event::Ui(Action { tag: ActionTag::Increment, .. })
+                => self.count += 1,
+            Event::Ui(Action { tag: ActionTag::Decrement, .. })
+                => self.count -= 1,
+            Event::Ui(Action { tag: ActionTag::SubmitMsg, associated, .. })
+                => self.msg = associated.get("value").unwrap().clone(),
         }
         true
     }
@@ -129,10 +141,12 @@ impl State {
 }
 
 pub fn new(handle: Handle) -> (impl Sink<SinkItem = Action, SinkError = ()>, impl Stream<Item = VNode<Action>, Error = ()>) {
-    let state = State { count: 0, msg: "".into() };
-    let (incoming_tx, incoming_rx) = mpsc::channel(1);
-    let (outgoing_tx, outgoing_rx) = mpsc::channel(1);
+    let (events, renders) = State::spawn(handle.clone());
+    let (ui, actions) = mpsc::channel(1);
 
-    handle.spawn(state.with_channels(incoming_rx, outgoing_tx));
-    (incoming_tx.sink_map_err(|e| println!("error sinking action: {:?}", e)), outgoing_rx)
+    let events = events.with(|e| { println!("event: {:?}", e); e });
+    let renders = renders.map(|r| { println!("render: {:?}", r); r });
+
+    handle.spawn(actions.map(|action| Ok(Event::Ui(action))).forward(events).map(|_| ()));
+    (ui.sink_map_err(|e| println!("error sinking action: {:?}", e)), renders)
 }
